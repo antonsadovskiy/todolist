a3e37b750180f0fc640052b1e9028565fa8e533a
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-01-25 23:47:58 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
                     <button>+</button>
                 </div>
                 <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML</span></li>
+                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
                     <li><input type="checkbox" checked={true}/> <span>JS</span></li>
                     <li><input type="checkbox" checked={false}/> <span>React</span></li>
                 </ul>

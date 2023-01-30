f51d8f66262eb9579d17649d5c357ff4fd872e15
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-01-30 21:05:43 +0300

    created task type and added him in todolist props type

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 type TodoListPropsType = {
-    title: string
+    title: string,
+    tasks: Array<TaskType>,
+}
+
+export type TaskType = {
+    id: number,
+    title: string,
+    isDone: boolean,
 }
 
 const TodoList = (props: TodoListPropsType) => {
@@ -13,9 +20,18 @@ const TodoList = (props: TodoListPropsType) => {
                 <button>+</button>
             </div>
             <ul>
-                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                <li>
+                    <input type="checkbox" checked={props.tasks[0].isDone}/>
+                    <span> {props.tasks[0].title} </span>
+                </li>
+                <li>
+                    <input type="checkbox" checked={props.tasks[1].isDone}/>
+                    <span> {props.tasks[1].title}</span>
+                </li>
+                <li>
+                    <input type="checkbox" checked={props.tasks[2].isDone}/>
+                    <span> {props.tasks[2].title}</span>
+                </li>
             </ul>
             <div>
                 <button>All</button>

b2c034a7f0a572a32f5e304c9a964eec8ced31ca
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-01-30 20:36:42 +0300

    create props type

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-const TodoList = () => {
+type TodoListPropsType = {
+    title: string
+}
+
+const TodoList = (props: TodoListPropsType) => {
     return (
         <div>
-            <h3>What to learn</h3>
+            <h3>{props.title}</h3>
             <div>
                 <input/>
                 <button>+</button>

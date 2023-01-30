cfb920fd90f8feb9a97a37fad19eb184d704356a
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-01-30 21:06:59 +0300

    created variable for todolist tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
-import TodoList from "./components/TodoList";
+import TodoList, {TaskType} from "./components/TodoList";
 
 const App = () => {
+
+    const todoListTitle: string = "Homework"
+
+    const todoListTasks: Array<TaskType> = [
+        {id: 1, title: "React", isDone: false},
+        {id: 2, title: "JS & TS", isDone: true},
+        {id: 3, title: "Math", isDone: true},
+    ]
+
     return (
         <div className="App">
-            <TodoList title={"Homework"}/>
-            <TodoList title={"Exams"}/>
-            <TodoList title={"Work"}/>
+            <TodoList title={todoListTitle} tasks={todoListTasks}/>
         </div>
     );
 }

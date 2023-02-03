33ae0c4595b9d27fb011beca4213c891ff979e28
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-02-04 00:08:26 +0300

    removed span

--- src/components/TodoLists/TodoList/TasksList/TasksList.tsx
+++ src/components/TodoLists/TodoList/TasksList/TasksList.tsx
@@ -7,13 +7,11 @@ type TaskListPropsType = {
 
 const TasksList:FC<TaskListPropsType> = (props) => {
 
-    const tasks: JSX.Element[] | JSX.Element = props.tasks.length
-        ? props.tasks.map((item) =>
+    const tasks: JSX.Element[] | JSX.Element = props.tasks.map((item) =>
             <li key={item.id}>
                 <input type="checkbox" checked={item.isDone}/>
                 <span> {item.title}</span>
             </li>)
-        : <span>Your tasks list is empty</span>
 
     return (
         <ul>

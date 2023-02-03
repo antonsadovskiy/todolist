22f56594e1b74c8f67b2fbdde949f2ea2b73569d
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-02-03 23:55:02 +0300

    created Data type, List type, Task type & added data in todolists, created todolists component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,57 @@
 import React from 'react';
 import './App.css';
-import TodoList, {TaskType} from "./components/TodoList";
+import TodoLists from "./components/TodoLists/TodoLists";
+
+export type DataType = {
+    lists: Array<ListType>
+}
+
+export type ListType = {
+    title: string
+    tasks: Array<TaskType>
+}
+
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
 
 const App = () => {
 
-    const todoListTitle: string = "Homework"
+    const data = {
+        lists: [
+            {
+                title: "Homework",
+                tasks: [
+                    {id: 1, title: "React", isDone: false},
+                    {id: 2, title: "JS & TS", isDone: true},
+                    {id: 3, title: "Math", isDone: true},
+                ]
+            },
+            {
+                title: "Exams",
+                tasks: [
+                    {id: 1, title: "React", isDone: false},
+                    {id: 2, title: "JS & TS", isDone: true},
+                    {id: 3, title: "Math", isDone: true},
+                ]
+            },
+            {
+                title: "Work",
+                tasks: [
+                    {id: 1, title: "React", isDone: false},
+                    {id: 2, title: "JS & TS", isDone: true},
+                    {id: 3, title: "Math", isDone: true},
+                ]
+            },
 
-    const todoListTasks: Array<TaskType> = [
-        {id: 1, title: "React", isDone: false},
-        {id: 2, title: "JS & TS", isDone: true},
-        {id: 3, title: "Math", isDone: true},
-    ]
+        ]
+    }
 
     return (
         <div className="App">
-            <TodoList title={todoListTitle} tasks={todoListTasks}/>
+            <TodoLists data={data}/>
         </div>
     );
 }

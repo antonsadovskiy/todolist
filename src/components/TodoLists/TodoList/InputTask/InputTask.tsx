75bc43b3119e3e8cf09cc5096c3916d11aef42e3
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-02-04 23:31:36 +0300

    created InputTask component

---
+++ src/components/TodoLists/TodoList/InputTask/InputTask.tsx
@@ -0,0 +1,30 @@
+import React, {ChangeEvent, FC, useState} from 'react';
+import s from './InputTask.module.css'
+
+type InputTaskPropsType = {
+    addTask: (value: string) => void
+}
+
+const InputTask:FC<InputTaskPropsType> = (props) => {
+
+    let [text, setText] = useState('')
+
+    const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setText(event.currentTarget.value)
+    }
+
+    const onClickButtonHandler = () => {
+        props.addTask(text)
+        setText('')
+    }
+
+
+    return (
+        <div className={s.input}>
+            <input onChange={onChangeInputHandler} value={text}/>
+            <button onClick={onClickButtonHandler}>+</button>
+        </div>
+    );
+};
+
+export default InputTask;

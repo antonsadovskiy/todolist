bbdbdb5a813a061bbcf17d571b41586c89a8e1c1
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-04-22 17:29:45 +0300

    added snapshot testing

---
+++ integration/app.test.js
@@ -0,0 +1,58 @@
+describe('App', () => {
+    it('input testing', async () => {
+        // APIs from jest-puppeteer
+        // eslint-disable-next-line no-undef
+        await page.goto('http://localhost:6006/iframe.html?args=&id=todolist-input--input-example&viewMode=story')
+
+        // eslint-disable-next-line no-undef
+        await page.waitForTimeout(3000)
+
+        // eslint-disable-next-line no-undef
+        const image = await page.screenshot()
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot()
+    })
+    it('completed task testing', async () => {
+        // APIs from jest-puppeteer
+        // eslint-disable-next-line no-undef
+        await page.goto('http://localhost:6006/iframe.html?args=&id=todolist-task--completed-task-example&viewMode=story')
+
+        // eslint-disable-next-line no-undef
+        await page.waitForTimeout(3000)
+
+        // eslint-disable-next-line no-undef
+        const image = await page.screenshot()
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot()
+    })
+    it('active task testing', async () => {
+        // APIs from jest-puppeteer
+        // eslint-disable-next-line no-undef
+        await page.goto('http://localhost:6006/iframe.html?args=&id=todolist-task--active-task-example&viewMode=story')
+
+        // eslint-disable-next-line no-undef
+        await page.waitForTimeout(3000)
+
+        // eslint-disable-next-line no-undef
+        const image = await page.screenshot()
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot()
+    })
+    it('buttons testing', async () => {
+        // APIs from jest-puppeteer
+        // eslint-disable-next-line no-undef
+        await page.goto('http://localhost:6006/iframe.html?args=&id=todolist-buttons--buttons-example&viewMode=story')
+
+        // eslint-disable-next-line no-undef
+        await page.waitForTimeout(3000)
+
+        // eslint-disable-next-line no-undef
+        const image = await page.screenshot()
+
+        // API from jest-image-snapshot
+        expect(image).toMatchImageSnapshot()
+    })
+})
